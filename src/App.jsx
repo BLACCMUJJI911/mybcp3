9005799a80bfd0222f3537deab7fefdfb3df4c3b
BLACCMUJJI911 <arieskm70@gmail.com>
2023-05-16 00:56:34 +0500

    class2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header";
+//import "./Header.css"
+
+let mainApp = () => {
+
+  return (
+  <div> 
+  <Header/>
+  {/* <Preview/> */}
+  </div>
+  )
+}
+export default mainApp;
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css"
+
+let Header = () => {
+    return (
+        <>
+            <div className="nav">
+                
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Gallery</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            </>
+    )
+}
+
+export default Header
